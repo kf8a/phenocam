baa980b48467bd6355cce69218729b15aeb73b2d
Sven Bohm <bohms@msu.edu>
2015-07-11 19:20:17 -0500

    use file_path and fixed format

--- camera.rb
+++ camera.rb
@@ -7,13 +7,13 @@ def take_picture
 
   plot = 'g2' #ARGV[0]
   rotation = '0' #ARGV[1]
-  at = Time.now.strftime("%Y-%m-%dT%H:%M:%S%")
+  at = Time.now.strftime("%Y-%m-%dT%H:%M:%S")
   file_path = "images/#{plot}-#{at}.jpg"
 
   if calc.compute_official_sunrise('America/New_York') < DateTime.now  && DateTime.now < calc.compute_official_sunset('America/New_York')
     cmd = system "raspistill -rot #{rotation} -awb off -awbg 1.4,1.5 -o #{file_path}"
     if cmd
-      msg = {at: at, location: plot, file: "images/#{plot}-#{at}"}
+      msg = {at: at, location: plot, file:  file_path}
       send_message(msg)
     end
   end
