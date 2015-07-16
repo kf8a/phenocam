6e76fdf8ec4d4c34018f73fab85b828b21521a68
Sven Bohm <bohms@msu.edu>
2015-07-15 20:40:36 -0700

    working except for g5 and 6

--- collector.rb
+++ collector.rb
@@ -10,6 +10,7 @@ def collect_data(camera)
   queue    = channel.queue("images2", :durable => true)
 
   queue.subscribe(:ack => true) do |metadata, payload|
+    begin
     data = YAML.load(payload)
     p data
     year = DateTime.parse(data[:at]).year
@@ -27,18 +28,22 @@ def collect_data(camera)
     File.exist?("/var/www/phenology/phenocam/#{year}/#{local_file}")
     File.exist?(current_file_name)
     File.symlink("/var/www/phenology/phenocam/#{year}/#{local_file}", current_file_name)
+    rescue e
+        metadata.ack
+	p e
+    end
   end
-
 
   Signal.trap "TERM", Proc.new { connection.close { EventMachine.stop } }
 end
 
+
 EventMachine.run do 
-  cameras = JSON.parse(open('http://oshtemo.kbs.msu.edu:8080/status').read)
+  cameras = ['35.13.15.44', '35.13.12.151', '35.13.14.52','35.13.14.47','35.13.12.204','35.13.13.228','35.13.13.164']
   cameras.each do |camera|
-    ip =  camera[1]["ip"]
+    ip =  camera
     collect_data(ip)
   end
 
-  EventMachine.add_timer(5*60) { EventMachine.stop }
+  EventMachine.add_timer(10*60) { EventMachine.stop }
 end
